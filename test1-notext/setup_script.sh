73eaa38e000a9eba09bdcbb1021214e2c93902d4
Devin J. McConnell <devin.mcconnell@uconn.edu>
2021-12-14 15:18:30 -0500

    Delete test1-notext directory
    The initial test run does not need to be here

--- test1-notext/setup_script.sh
+++
@@ -1,7 +0,0 @@
-dir=(/home/FCAM/dmcconnell/Social_media_mining/data_files/final*.csv)
-
-for ((i=0; i<${#dir[@]}; i++)); do
-	for j in {0..6}; do
-		echo 'python3 model_search.py '${dir[$i]} ${j} >> submit_classification_jobs.sh
-	done
-done
--- test1-notext/submit_classification_jobs.sh
+++
@@ -1,7 +0,0 @@
-python3 model_search.py /home/FCAM/dmcconnell/Social_media_mining/data_files/final_data-2.csv 0
-python3 model_search.py /home/FCAM/dmcconnell/Social_media_mining/data_files/final_data-2.csv 1
-python3 model_search.py /home/FCAM/dmcconnell/Social_media_mining/data_files/final_data-2.csv 2
-python3 model_search.py /home/FCAM/dmcconnell/Social_media_mining/data_files/final_data-2.csv 3
-python3 model_search.py /home/FCAM/dmcconnell/Social_media_mining/data_files/final_data-2.csv 4
-python3 model_search.py /home/FCAM/dmcconnell/Social_media_mining/data_files/final_data-2.csv 5
-python3 model_search.py /home/FCAM/dmcconnell/Social_media_mining/data_files/final_data-2.csv 6
